1e26b2b1192f542a4f3947a9f95f36214a414bb7
Jimmy <jimmy.sobandith@outlook.fr>
2017-12-12 15:27:12 +0100

    Exercice 2

--- src/shop-tools.js
+++ src/shop-tools.js
@@ -1,4 +1,11 @@
+const product = [ 'tomato', 'cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato', 'potato', 'tomato', 'potato' ];
+
 exports.getPrices = function()
 {
 	return {banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6};
+}
+
+exports.countArticles = function(product)
+{
+	return product.length;
 }
--- test/shop-tools.test.js
+++ test/shop-tools.test.js
@@ -1,8 +1,14 @@
 var expect = require('chai').expect;
 var getPrices = require('../src/shop-tools').getPrices;
+var countArticles = require('../src/shop-tools').countArticles;
+const product = [ 'tomato', 'cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato', 'potato', 'tomato', 'potato' ];
 
-describe('Function retourne prix', function() {
-    it('Retourne le prix du marcher', function() {
+
+describe('Mocha Projet', function() {
+    it('Exercice 1: Retourne le prix du marcher', function() {
         expect(getPrices()).to.eql({banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6});
+    })
+	it('Exercice 2: Retourne le nombre article dans un tableau', function() {
+        expect(countArticles(product)).to.eql(10);
     })
 });
